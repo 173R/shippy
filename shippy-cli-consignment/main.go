444af5dc950fa0e9bbc9a748cb4bae470b1bbb22
artem <artyom.getman1996@gmail.com>
2022-11-19 19:12:47 +1000

    init

---
+++ shippy-cli-consignment/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"google.golang.org/grpc"
+	"log"
+	"os"
+	pb "shippy/shippy-service-consignment/proto/consignment"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+// Парсим коносамент который хранится в json тут на клиенте
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+	//Коннектимся к серверу
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewShippingServiceClient(conn)
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+
+	res, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+	log.Printf("Created: %t", res.Created)
+
+	getAll, err := client.GetConsignments(
+		context.Background(), &pb.GetRequest{},
+	)
+
+	if err != nil {
+		log.Fatalf("Could not list consignments: %v", err)
+	}
+
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
+}
---
+++ shippy-service-consignment/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	pb "shippy/shippy-service-consignment/proto/consignment"
+	"sync"
+)
+
+const port = ":50051"
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+type Repository struct {
+	mu           sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+// Создание коносамента
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.mu.Unlock()
+	return consignment, nil
+}
+
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+type service struct {
+	repo repository
+	pb.UnimplementedShippingServiceServer
+}
+
+// Создаём методы которые отражают методы из proto
+func (s *service) CreateConsignment(
+	ctx context.Context,
+	req *pb.Consignment,
+) (*pb.Response, error) {
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func (s *service) GetConsignments(
+	context.Context,
+	*pb.GetRequest,
+) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
+}
+
+func main() {
+	repo := &Repository{}
+
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterShippingServiceServer(grpcServer, &service{repo: repo})
+
+	reflection.Register(grpcServer)
+
+	log.Println("Running on port:", port)
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
